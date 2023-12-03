e48a4c5b2356c5d5192e73ec22f986db3b03cf4d
Shannon Moeller <me@shannonmoeller.com>
2023-12-03 02:32:54 +0000

    2023/02.2

--- 2023/02-1.js
+++ 2023/02-1.js
@@ -5,24 +5,17 @@ const file = await open('./02.txt');
 let value = 0;
 
 for await (const line of file.readLines()) {
-	if (line.match(/(2\d|1[3-9]) red/)) {
-		continue;
-	}
-
-	if (line.match(/(2\d|1[4-9]) green/)) {
-		continue;
-	}
-
-	if (line.match(/(2\d|1[5-9]) blue/)) {
+	if (
+		line.match(/(2\d|1[3-9]) red/) ||
+		line.match(/(2\d|1[4-9]) green/) ||
+		line.match(/(2\d|1[5-9]) blue/)
+	) {
 		continue;
 	}
 
 	const [, id] = line.match(/Game (\d+)/);
-	const number = Number(id);
 
-	console.log(line, id, number);
-
-	value += number;
+	value += Number(id);
 }
 
 console.log(value);
