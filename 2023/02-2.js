4d596827c804c12bf5e7a5b58860e2f4901cd156
Shannon Moeller <me@shannonmoeller.com>
2023-12-03 02:23:39 +0000

    2023/02.1

--- 2023/02-2.js
+++ 2023/02-2.js
@@ -3,22 +3,16 @@ import { open } from 'node:fs/promises';
 const file = await open('./02.txt');
 
 function getMax(line, rx) {
-	const numbers = line.match(rx) ?? [];
-
-	return Math.max(...numbers) || 0;
+	return Math.max(...line.match(rx));
 }
 
 let value = 0;
 
 for await (const line of file.readLines()) {
-	const reds = getMax(line, /\d+(?= red)/g);
-	const greens = getMax(line, /\d+(?= green)/g);
-	const blues = getMax(line, /\d+(?= blue)/g);
-	const power = reds * greens * blues;
-
-	console.log({ line, reds, greens, blues, power });
-
-	value += power;
+	value +=
+		getMax(line, /\d+(?= red)/g) *
+		getMax(line, /\d+(?= green)/g) *
+		getMax(line, /\d+(?= blue)/g);
 }
 
 console.log(value);
