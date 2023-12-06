2a2977d9e5e0bc4609e3414aa37d7ba348d11c33
Shannon Moeller <me@shannonmoeller.com>
2023-12-06 19:46:11 +0000

    2023/06

---
+++ 2023/06-1.js
@@ -0,0 +1,33 @@
+import { log, readLines } from './utils.js';
+
+let lines = readLines('./06.txt');
+let value = 1;
+
+let [times, dists] = lines.map((x) => x.match(/\d+/g).map(Number));
+
+// Binary search would be better, but this is less code
+for (let i = 0; i < times.length; i++) {
+	let time = times[i];
+	let dist = dists[i];
+	let d;
+
+	let low = 1;
+
+	while (true) {
+		d = low * (time - low);
+		if (d > dist) break;
+		low++;
+	}
+
+	let high = time - 1;
+
+	while (true) {
+		d = high * (time - high);
+		if (d > dist) break;
+		high--;
+	}
+
+	value *= high - low + 1;
+}
+
+console.log(value);
