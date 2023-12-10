936930bb21e0f8cb2face1b5e7e58e3e1154aebf
Shannon Moeller <me@shannonmoeller.com>
2023-12-10 00:18:03 +0000

    2023/09.1

--- 2023/09-1.js
+++ 2023/09-1.js
@@ -3,16 +3,16 @@ import { log, readLines } from './utils.js';
 let lines = readLines('./09.txt');
 let value = 0;
 
-function sumNthDiff(nums) {
+function sumNext(nums) {
 	return nums.at(-1) + (
-		new Set(nums).size === 1 ? 0 : sumNthDiff(
+		new Set(nums).size === 1 ? 0 : sumNext(
 			nums.slice(1).map((x, i) => x - nums[i])
 		)
 	);
 }
 
 for (let line of lines) {
-	value += sumNthDiff(line.split(/\s+/).map(Number));
+	value += sumNext(line.split(/\s+/).map(Number));
 }
 
 console.log(value);
