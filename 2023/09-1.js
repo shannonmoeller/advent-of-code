10a45d69f59b5003f714a0a3f92ba3e01fa17299
Shannon Moeller <me@shannonmoeller.com>
2023-12-10 01:45:17 +0000

    2023/09.2

--- 2023/09-1.js
+++ 2023/09-1.js
@@ -3,16 +3,16 @@ import { log, readLines } from './utils.js';
 let lines = readLines('./09.txt');
 let value = 0;
 
-function sumNext(nums) {
+function next(nums) {
 	return nums.at(-1) + (
-		new Set(nums).size === 1 ? 0 : sumNext(
-			nums.slice(1).map((x, i) => x - nums[i])
+		new Set(nums).size === 1 ? 0 : next(
+			nums.slice(1).map((n, i) => n - nums[i])
 		)
 	);
 }
 
 for (let line of lines) {
-	value += sumNext(line.split(/\s+/).map(Number));
+	value += next(line.split(/\s+/).map(Number));
 }
 
 console.log(value);
--- 2023/09-2.js
+++ 2023/09-2.js
@@ -3,16 +3,16 @@ import { log, readLines } from './utils.js';
 let lines = readLines('./09.txt');
 let value = 0;
 
-function sumPrev(nums) {
+function prev(nums) {
 	return nums[0] - (
-		new Set(nums).size === 1 ? 0 : sumPrev(
-			nums.slice(1).map((x, i) => x - nums[i])
+		new Set(nums).size === 1 ? 0 : prev(
+			nums.slice(1).map((n, i) => n - nums[i])
 		)
 	);
 }
 
 for (let line of lines) {
-	value += sumPrev(line.split(/\s+/).map(Number));
+	value += prev(line.split(/\s+/).map(Number));
 }
 
 console.log(value);
