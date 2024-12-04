4eda85f8eca441737b2f6643c1e7d5d8523ca51f
Shannon Moeller <me@shannonmoeller.com>
2024-12-03 22:47:49 -0700

    2024/04

--- 2024/04-1.js
+++ 2024/04-1.js
@@ -3,13 +3,27 @@ import { exec, log } from './utils.js';
 function main(lines) {
   let value = 0;
 
-  for (let line of lines) {
-    log({ line });
+  function walk(x, y, xd, yd, [next, ...rest]) {
+    let node = lines[y]?.[x];
+
+    if (node !== next) return;
+    if (!rest.length) return value++;
+
+    walk(x + xd, y + yd, xd, yd, rest);
+  }
+
+  for (let y = 0; y < lines.length; y++) {
+    for (let x = 0; x < lines[0].length; x++) {
+      for (let yd = -1; yd <= 1; yd++) {
+        for (let xd = -1; xd <= 1; xd++) {
+          walk(x, y, xd, yd, ['X', 'M', 'A', 'S']);
+        }
+      }
+    }
   }
 
   return value;
 }
 
-exec('./04-a.txt', main, 0);
-// exec('./04-1.txt', main);
-
+exec('./04-a.txt', main, 18);
+exec('./04-1.txt', main);
