7ba8b3682aac5aebce834bbfd85d35524aa5d5eb
Shannon Moeller <me@shannonmoeller.com>
2024-12-09 22:44:52 -0700

    2024/10

---
+++ 2024/10-1.js
@@ -0,0 +1,42 @@
+import { exec, splitMap } from './utils.js';
+
+function main(lines) {
+  let value = 0;
+
+  let trailheads = lines.join('').matchAll(/0/g);
+  let width = lines[0].length;
+
+  for (let trailhead of trailheads) {
+    let x = trailhead.index % width;
+    let y = (trailhead.index / width) | 0;
+    let summits = {};
+
+    function walk(x, y, prev) {
+      let node = +lines[y]?.[x];
+
+      if (node == null) return;
+      if (node !== prev + 1) return;
+      if (node === 9) {
+        if (!summits[y]?.[x]) value++;
+        (summits[y] ??= {})[x] = true;
+      }
+
+      walk(x + 1, y, node);
+      walk(x - 1, y, node);
+      walk(x, y + 1, node);
+      walk(x, y - 1, node);
+    }
+
+    walk(x + 1, y, 0);
+    walk(x - 1, y, 0);
+    walk(x, y + 1, 0);
+    walk(x, y - 1, 0);
+  }
+
+  return value;
+}
+
+exec('./10-a.txt', main, 2);
+exec('./10-b.txt', main, 4);
+exec('./10-c.txt', main, 36);
+exec('./10-1.txt', main);
---
+++ 2024/10-2.js
@@ -0,0 +1,38 @@
+import { exec, splitMap } from './utils.js';
+
+function main(lines) {
+  let value = 0;
+
+  let trailheads = lines.join('').matchAll(/0/g);
+  let width = lines[0].length;
+
+  for (let trailhead of trailheads) {
+    let x = trailhead.index % width;
+    let y = (trailhead.index / width) | 0;
+
+    function walk(x, y, prev) {
+      let node = +lines[y]?.[x];
+
+      if (node == null) return;
+      if (node !== prev + 1) return;
+      if (node === 9) value++;
+
+      walk(x + 1, y, node);
+      walk(x - 1, y, node);
+      walk(x, y + 1, node);
+      walk(x, y - 1, node);
+    }
+
+    walk(x + 1, y, 0);
+    walk(x - 1, y, 0);
+    walk(x, y + 1, 0);
+    walk(x, y - 1, 0);
+  }
+
+  return value;
+}
+
+exec('./10-a.txt', main, 2);
+exec('./10-b.txt', main, 13);
+exec('./10-c.txt', main, 81);
+exec('./10-1.txt', main);
