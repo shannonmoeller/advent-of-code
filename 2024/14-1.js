a27ed0551b98565b94aab34e203075a2c7845731
Shannon Moeller <me@shannonmoeller.com>
2024-12-14 10:12:02 -0700

    2024/14

--- 2024/14-1.js
+++ 2024/14-1.js
@@ -1,14 +1,33 @@
-import { exec, log } from '../utils.js';
+import { exec } from '../utils.js';
 
-function main(lines) {
-  let value = 0;
+function main(s, w, h) {
+  return (lines) => {
+    let wh = Math.floor(w / 2);
+    let hh = Math.floor(h / 2);
 
-  for (let line of lines) {
-    log({ line });
-  }
+    let a = 0;
+    let b = 0;
+    let c = 0;
+    let d = 0;
 
-  return value;
+    for (let line of lines) {
+      let [ax, ay, vx, vy] = line.match(/-?\d+/g).map(Number);
+      let bx = (ax + vx * s + w * s) % w;
+      let by = (ay + vy * s + h * s) % h;
+
+      if (bx < wh) {
+        if (by < hh) a++;
+        if (by > hh) b++;
+      }
+      if (bx > wh) {
+        if (by < hh) c++;
+        if (by > hh) d++;
+      }
+    }
+
+    return a * b * c * d;
+  };
 }
 
-exec(main, './14-a.txt', 0);
-// exec(main, './14-1.txt');
+exec(main(100, 11, 7), './14-a.txt', 12);
+exec(main(100, 101, 103), './14-1.txt');
